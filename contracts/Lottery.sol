efafe7b9c3b7bbf1872ff8d9e9d0435d2b594121
Santiago Del Valle <sdelvalle57@gmail.com>
2018-05-01 15:34:41 +1000

    Writing unit tests on funtion enter

--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -2,21 +2,30 @@ pragma solidity ^0.4.21;
 
 import "./ownership/Ownable.sol";
 
+/**
+ * @title Lottery
+ * @dev Lottery with 6 numbers between 01-99.
+ */
 contract Lottery is Ownable {
+    
+    /**
+    * @dev mapping of the LotteryNumber => users who chose that number
+    */
+    mapping(uint256 => address[]) public contest;
+    address[] public players;
 
-    struct Player {
-        address playerAddress;
+    function enter(uint256 _number) external payable {
+        require(msg.value > 0 ether);
+        players.push(msg.sender);
+        contest[_number].push(msg.sender);    
     }
 
-    Player[] public players;
-    mapping(uint256 => Player[]) public winners;
+    function getPlayers() external view returns (address[]) {
+        return players;
+    }
 
-    function enter() external payable {
-        require(msg.value > 2 ether);
-        Player memory player = Player({
-            playerAddress: msg.sender
-        });
-        players.push(player);        
+    function getPlayer(uint256 _number) external view returns (address[]) {
+        return contest[_number];
     }
 
 }
---
+++ test/test.js
@@ -0,0 +1,148 @@
+var Web3 = require('web3');
+var web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
+let Lottery = artifacts.require('./Lottery.sol');
+
+let lottery;
+let accounts;
+beforeEach(async () =>{
+    accounts = await web3.eth.getAccounts();
+    lottery = await new web3.eth.Contract(JSON.parse(JSON.stringify(Lottery.abi)))
+        .deploy({data: Lottery.bytecode})
+        .send({from: accounts[0], gas: '1000000'});   
+});
+
+contract('Lottery', () =>{
+    it('deploys a contract', ()=>{
+        assert.ok(lottery.options.address);
+    });
+
+    it('allows one account to enter', async () =>{
+        const numberOfLottery = 123456;
+        await lottery.methods.enter(numberOfLottery).send({
+            from: accounts[0],
+            value: web3.utils.toWei('3', 'ether'),
+            gas: '1000000'
+        });
+        const players = await lottery.methods.getPlayers().call();
+        const playersByNumber = await lottery.methods.getPlayer(numberOfLottery).call();
+        assert.equal(accounts[0], players[0]);
+        assert.equal(accounts[0], playersByNumber[0]);
+        assert.equal(1, players.length);
+    });
+    it('allows five accounts to enter with one repeated number', async () =>{
+        const numberOfLottery1 = 123456;
+        const numberOfLottery2 = 654321;
+        const numberOfLottery3 = 987654;
+        const numberOfLottery4 = 456789;
+        const numberOfLottery5 = 123456;
+        
+        await lottery.methods.enter(numberOfLottery1).send({
+            from: accounts[0],
+            value: web3.utils.toWei('3', 'ether'),
+            gas: '1000000'
+        });
+        await lottery.methods.enter(numberOfLottery2).send({
+            from: accounts[1],
+            value: web3.utils.toWei('3', 'ether'),
+            gas: '1000000'
+        });
+        await lottery.methods.enter(numberOfLottery3).send({
+            from: accounts[2],
+            value: web3.utils.toWei('3', 'ether'),
+            gas: '1000000'
+        });
+        await lottery.methods.enter(numberOfLottery4).send({
+            from: accounts[3],
+            value: web3.utils.toWei('3', 'ether'),
+            gas: '1000000'
+        });
+        await lottery.methods.enter(numberOfLottery5).send({
+            from: accounts[4],
+            value: web3.utils.toWei('3', 'ether'),
+            gas: '1000000'
+        });
+        const players = await lottery.methods.getPlayers().call();
+        assert.equal(accounts[0], players[0]);
+        assert.equal(accounts[1], players[1]);
+        assert.equal(accounts[2], players[2]);
+        assert.equal(accounts[3], players[3]);
+        assert.equal(accounts[4], players[4]);
+        assert.equal(5, players.length);
+        let playersByNumber = await lottery.methods.getPlayer(numberOfLottery1).call();
+        assert.equal(2, playersByNumber.length);
+        assert.equal(accounts[0], playersByNumber[0]);
+        assert.equal(accounts[4], playersByNumber[1]);
+        playersByNumber = await lottery.methods.getPlayer(numberOfLottery2).call();
+        assert.equal(1, playersByNumber.length);
+        playersByNumber = await lottery.methods.getPlayer(numberOfLottery3).call();
+        assert.equal(1, playersByNumber.length);
+        playersByNumber = await lottery.methods.getPlayer(numberOfLottery4).call();
+        assert.equal(1, playersByNumber.length);
+        playersByNumber = await lottery.methods.getPlayer(numberOfLottery5).call();
+        assert.equal(2, playersByNumber.length);
+        assert.equal(accounts[0], playersByNumber[0]);
+        assert.equal(accounts[4], playersByNumber[1]);    
+    });
+    it('allows one account to enter several times', async () =>{
+        const numberOfLottery1 = 123456;
+        const numberOfLottery2 = 654321;
+        const numberOfLottery3 = 987654;
+
+        await lottery.methods.enter(numberOfLottery1).send({
+            from: accounts[0],
+            value: web3.utils.toWei('3', 'ether'),
+            gas: '1000000'
+        });
+        await lottery.methods.enter(numberOfLottery2).send({
+            from: accounts[0],
+            value: web3.utils.toWei('3', 'ether'),
+            gas: '1000000'
+        });
+        await lottery.methods.enter(numberOfLottery3).send({
+            from: accounts[0],
+            value: web3.utils.toWei('3', 'ether'),
+            gas: '1000000'
+        });
+        const players = await lottery.methods.getPlayers().call();
+        assert.equal(accounts[0], players[0]);
+        assert.equal(accounts[0], players[1]);
+        assert.equal(accounts[0], players[2]);
+        assert.equal(3, players.length);
+        let playersByNumber = await lottery.methods.getPlayer(numberOfLottery1).call();
+        assert.equal(accounts[0], playersByNumber[0]);
+        playersByNumber = await lottery.methods.getPlayer(numberOfLottery2).call();
+        assert.equal(accounts[0], playersByNumber[0]);
+        playersByNumber = await lottery.methods.getPlayer(numberOfLottery3).call();
+        assert.equal(accounts[0], playersByNumber[0]);        
+    });
+    it('allows one account to enter several times the same number', async () =>{
+        const numberOfLottery = 123456;
+        await lottery.methods.enter(numberOfLottery).send({
+            from: accounts[0],
+            value: web3.utils.toWei('3', 'ether'),
+            gas: '1000000'
+        });
+        await lottery.methods.enter(numberOfLottery).send({
+            from: accounts[0],
+            value: web3.utils.toWei('3', 'ether'),
+            gas: '1000000'
+        });
+        await lottery.methods.enter(numberOfLottery).send({
+            from: accounts[0],
+            value: web3.utils.toWei('3', 'ether'),
+            gas: '1000000'
+        });
+        const players = await lottery.methods.getPlayers().call();
+        assert.equal(accounts[0], players[0]);
+        assert.equal(accounts[0], players[1]);
+        assert.equal(accounts[0], players[2]);
+        assert.equal(3, players.length);
+        const playersByNumber = await lottery.methods.getPlayer(numberOfLottery).call();
+        assert.equal(accounts[0], playersByNumber[0]);
+        assert.equal(accounts[0], playersByNumber[1]);
+        assert.equal(accounts[0], playersByNumber[2]);
+        assert.equal(3, playersByNumber.length);
+        
+    });
+})
+
