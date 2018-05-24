e439ceb0c169631aa6b004364e59beadd4a8ccea
Santiago Del Valle <sdelvalle57@gmail.com>
2018-05-24 14:23:44 +1000

    setting server

--- server.js
+++ server.js
@@ -9,8 +9,8 @@ const routes = require('./routes');
 const handler = routes.getRequestHandler(app);
 
 app.prepare().then(() => {
-    createServer(handler).listen(80, (err) =>{
+    createServer(handler).listen(3000, (err) =>{
         if(err) throw err;
-        console.log('Ready on localhost:8080');
+        console.log('Ready on localhost:3000');
     });
 });
