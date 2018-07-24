939e8a3e889217cedef09af1f5e69aa6d63e2dfc
Santiago Del Valle <sdelvalle57@gmail.com>
2018-07-24 18:17:38 +1000

    make it better

--- ethereum/web3Socket.js
+++ ethereum/web3Socket.js
@@ -1,4 +1,4 @@
 import Web3 from 'web3';
 
-//export default new Web3(new Web3.providers.WebsocketProvider('wss://rinkeby.infura.io/_ws'));
-export default new Web3("ws://localhost:8545");
+export default new Web3(new Web3.providers.WebsocketProvider('wss://rinkeby.infura.io/_ws'));
+//export default new Web3("ws://localhost:8545");
