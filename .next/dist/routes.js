bb9c517e5f9638f2e1e2d1e20d08fd6b5a5f3518
Santiago Del Valle <sdelvalle57@gmail.com>
2018-07-24 14:19:13 +1000

    not working version

--- .next/dist/routes.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-var routes = require('next-routes')();
-
-routes.add('/lotteries/new/', '/index').add('/lotteries/new/:factoryAddress', '/lotteries/new').add('/lotteries/:lotteryAddress', '/lotteries/lottery');
-
-module.exports = routes;
